abc40c4ffc3e77e19c1e5b5348bd4648cff36f59
Aditya Tiwatane <adityatiwatane1998@gmail.com>
2022-09-01 08:37:37 +0000

    initial commit

---
+++ springin5steps/src/main/java/com/courses/spring/basics/springin5steps/Springin5stepsApplication.java
@@ -0,0 +1,13 @@
+package com.courses.spring.basics.springin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springin5stepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springin5stepsApplication.class, args);
+	}
+
+}
