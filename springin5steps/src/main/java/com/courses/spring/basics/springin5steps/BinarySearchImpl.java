65e7f0271d9a4d82fc0bbc8db844a2e54f3bf474
adityatiwatane <104667727+adityatiwatane@users.noreply.github.com>
2022-09-01 15:01:08 +0530

    java classes

---
+++ springin5steps/src/main/java/com/courses/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,14 @@
+package com.courses.spring.basics.springin5steps
+
+public class  BinarySearchImpl  {
+    public int binarySearch(int[] numbers, int numbersToSearchFor) {
+        //implementing sorting logic 
+        //search the array
+        //return the result
+        return 3;
+    }
+    //sorting an array 
+
+    //seARCH THE ARRAY
+    //search the result
+}
