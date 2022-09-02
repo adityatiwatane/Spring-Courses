83418ea8fd11f2439d48ce6fbafd66092fc1ff14
adityatiwatane <104667727+adityatiwatane@users.noreply.github.com>
2022-09-02 16:07:45 +0530

    t

--- springin5steps/src/main/java/com/courses/spring/basics/springin5steps/Springin5stepsApplication.java
+++ springin5steps/src/main/java/com/courses/spring/basics/springin5steps/Springin5stepsApplication.java
@@ -2,7 +2,7 @@ package com.courses.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Springin5stepsApplication {
