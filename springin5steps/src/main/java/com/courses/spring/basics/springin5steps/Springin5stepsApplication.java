e63b639723c77a0425305b6d5b3e29ceee9672aa
adityatiwatane <104667727+adityatiwatane@users.noreply.github.com>
2022-09-02 12:42:15 +0530

    java classes

--- springin5steps/src/main/java/com/courses/spring/basics/springin5steps/Springin5stepsApplication.java
+++ springin5steps/src/main/java/com/courses/spring/basics/springin5steps/Springin5stepsApplication.java
@@ -7,10 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Springin5stepsApplication {
 
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm);
-		int result = binarySearch.binarySearch(new int[] {12, 4, 6}, 3);
-		System.out.println(result);
-		//SpringApplication.run(Springin5stepsApplication.class, args);
+		//BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm);
+		//application context
+
+		ApplicationContext applicationContext = SpringApplication.run(Springin5stepsApplication.class, args);
+		BinarySearchImpl binarySearch = applicationContext.getBeans(BinarySearchImpl.class)
+	int result = binarySearch.binarySearch(new int[] {12, 4, 6}, 3);
+	System.out.println(result);
 	}
 
 }
