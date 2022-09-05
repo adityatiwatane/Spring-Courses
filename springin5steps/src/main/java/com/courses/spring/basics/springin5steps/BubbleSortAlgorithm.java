46f101b35cf956de3cc75213a8f22dad2c8b3302
adityatiwatane <104667727+adityatiwatane@users.noreply.github.com>
2022-09-05 12:46:12 +0530

    c

--- springin5steps/src/main/java/com/courses/spring/basics/springin5steps/BubbleSortAlgorithm.java
+++ springin5steps/src/main/java/com/courses/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -2,6 +2,7 @@ package com.courses.spring.basics.springin5steps;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class BubbleSortAlgorithm implements SortAlgorithm{
     public int[] sort(int[] numbers) {
         //logic for bubble sort 
--- springin5steps/src/main/java/com/courses/spring/basics/springin5steps/QuickSortAlgorithm.java
+++ springin5steps/src/main/java/com/courses/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -1,5 +1,6 @@
 package com.courses.spring.basics.springin5steps;
 
+@Component
 public class QuickSortAlgorithm implements SortAlgorithm{
     public int[] sort(int[] numbers) {
         //logic for quick sort
